5387e0375a1b38dd432526556f85c569f07ca816
Michael Ebert <michael.ebert@gmail.com>
2016-09-15 21:59:36 +0200

    Improved docs and pm2 config file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# sandbox-server
+# sandbox-server for trycoding.io
+
+## Install
+1. You need to setup a sourcebox template container. Use the sourcebox utils for that.
+2. `npm install`  the *sandbox-server*
+3. `npm install -g pm2`
+
+## Start Server
+Start the server with: `sudo pm2 start process.json --env production`
+
+### Debugging
+Start the server with `sudo DEBUG=*.sourcebox pm2 start process.json --env production` to get all the debug outputs on the console.
--- server.js
+++ server.js
@@ -4,12 +4,15 @@ var http = require('http');
 var express = require('express');
 var jwt = require('jsonwebtoken');
 var Config = require('config');
+var cors = require('cors');
 
 var app = express();
 
 if (Config.server.serveStatic === true) {
     app.use(express.static('public'));
 }
+
+app.use(cors());
 
 var httpServer = http.createServer(app);
 
@@ -22,7 +25,8 @@ var sourceboxServer = new Server(source, {
   auth: function (socket, token) {
     var user = jwt.verify(token, Config.auth.secret);
     return user.username || socket.handshake.address;
-  }
+  },
+  io: Config.sourcebox.io
 });
 
 sourceboxServer.attach(httpServer);
